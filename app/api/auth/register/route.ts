22574fab68142f264a9bca4b026a4cd1c97b53c2
June <dungngo.nntd@gmail.com>
2026-06-08 19:01:36 +0700

    feat: read register request body

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,8 +1,10 @@
-export async function POST() {
+export async function POST(request: Request) {
+  const body = await request.json();
   return Response.json(
     {
       success: true,
       message: "Register API is working.",
+      data: body,
     },
     {
       status: 200,
